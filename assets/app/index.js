f7372c1103b328132e5baf32e31f2a6eb545baac
Bibekpandey <spirit_bibek@yahoo.com>
2017-12-07 21:48:37 +0000

    add script to calculate accuracy and size of dataset

--- assets/app/index.js
+++ assets/app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../css/index.css';
 import Main from './components/Main';
-import { BrowserRouter, browserHistory } from 'react-router-dom'
+import { BrowserRouter, browserHistory } from 'react-router-dom';
 
 // ========================================
 
