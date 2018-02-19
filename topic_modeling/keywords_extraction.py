5d5501067f9f10708e11824fa1237adb340e0c5c
Bibekpandey <spirit_bibek@yahoo.com>
2018-02-19 11:22:11 +0000

    Add lemmatizer for keywords extraction as well

--- topic_modeling/keywords_extraction.py
+++ topic_modeling/keywords_extraction.py
@@ -1,17 +1,26 @@
 from rake_nltk import Rake
-from helpers.common import remove_punc_and_nums
+from helpers.common import remove_punc_and_nums, lemmatize
+
 
 def get_key_ngrams(document, max_grams=3, include_numbers=False):
-    r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
-    # r = Rake(<language>) # To use it in a specific language supported by nltk.
+    # Uses stopwords for english from NLTK, and all puntuation characters.
+    r = Rake()
+    # r=Rake(<language>) # To use it in a specific language supported by nltk.
     # If you want to provide your own set of stop words and punctuations to
     # r = Rake(<list of stopwords>, <string of puntuations to ignore>)
     if not include_numbers:
-        document = remove_punc_and_nums(document) # numbers are replaced by NN
-    r.extract_keywords_from_text(document.lower())
-    phrases = r.get_ranked_phrases_with_scores() # To get keyword phrases ranked highest to lowest.
+        document = remove_punc_and_nums(document)  # numbers are replaced by NN
 
-    data = { '{}grams'.format(x+1):[] for x in range(max_grams) }
+    document = document.lower()
+
+    # lemmatize
+    document = lemmatize(document)
+    r.extract_keywords_from_text(document)
+
+    # To get keyword phrases ranked highest to lowest.
+    phrases = r.get_ranked_phrases_with_scores()
+
+    data = {'{}grams'.format(x+1): [] for x in range(max_grams)}
 
     for score, phrase in phrases:
         if 'NN' in phrase:
