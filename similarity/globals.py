2458f211993ea02274b0ab46cedfc6944ea01a81
bewakes <bewakepandey@gmail.com>
2018-03-25 12:25:17 +0545

    exception handling

--- similarity/globals.py
+++ similarity/globals.py
@@ -5,7 +5,11 @@ __similarity_model = None
 
 def init():
     global __similarity_model
-    __similarity_model = DocumentSimilarityModel()
+    try:
+        __similarity_model = DocumentSimilarityModel()
+    except Exception:
+        # sometimes when indices haven't been created, exception can be thrown
+        pass
 
 
 def get_similarity_model():
