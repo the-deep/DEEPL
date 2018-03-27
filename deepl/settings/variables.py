5113521256abc07605695c01da6a20cc724b2bc6
bewakes <bewakepandey@gmail.com>
2018-03-27 12:43:33 +0545

    update settings variable

--- deepl/settings/variables.py
+++ deepl/settings/variables.py
@@ -2,6 +2,6 @@
 # Variable for storing index files for NLP
 INDICES_PATH_ENV_VAR = 'NLP_INDICES_PATH'
 
-TERM_FREQUENCY_INDEX_FILENAME = 'tf.index'
-INVERSE_FREQUENCY_INDEX_FILENAME = 'idf.index'
-TERM_INDEX_FILENAME = 'terms.index'
+TERM_FREQUENCY_INDEX_FILENAME = 'tf_compressed.json'
+INVERSE_FREQUENCY_INDEX_FILENAME = 'idf.json'
+TERM_INDEX_FILENAME = 'terms_index.json'
