57b30181cc05048a0ea4aaeb1a0e27218ac57117
bewakes <spirit_bibek@yahoo.com>
2018-04-15 17:33:55 +0545

    model for doc2vec

--- clustering/models.py
+++ clustering/models.py
@@ -1,9 +1,12 @@
 import pickle
+import os
 
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.conf import settings
 
 from classifier.models import BaseModel
+from helpers.utils import Resource
 
 
 class ClusteringModel(BaseModel):
@@ -26,3 +29,46 @@ class ClusteringModel(BaseModel):
 
     def __str__(self):
         return "{} - {}".format(self.name, self.version)
+
+
+class Doc2VecModel(BaseModel):
+    """
+    Model to store data about gensim doc2vec model created
+    """
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=20, unique=True, editable=False)
+    modelpath = models.CharField(max_length=500)
+
+    PATHTYPE_FILE = 'FILE'
+    PATHTYPE_URL = 'URL'
+
+    PATHTYPE_CHOICES = (
+        (PATHTYPE_FILE, 'File'),
+        (PATHTYPE_URL, 'Url')
+    )
+
+    pathtype = models.CharField(
+        max_length=50,
+        choices=PATHTYPE_CHOICES,
+        default=PATHTYPE_FILE
+    )
+
+    @classmethod
+    def new(cls, doc2vecmodel, name, version):
+        resource = Resource(
+            settings.DOC2VEC_MODELS_LOCATION,
+            Resource.DIRECTORY_AND_ENVIRONMENT
+        )
+        path = resource.get_resource_location()
+        doc2vec = cls(
+            name=name,
+            version=version,
+            modelpath=path
+        )
+        doc2vec.save()
+        filename = 'doc2vec_id_{}'.format(doc2vec.id)
+        doc2vec.modelpath = os.path.join(doc2vec.modelpath, filename)
+        doc2vec.save()
+        # finally save the doc2vec model in a file
+        doc2vecmodel.save(doc2vec.modelpath)
+        return doc2vec
