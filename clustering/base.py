8a86463a495edd9dd109b95d590798694f92ba0a
bewakes <spirit_bibek@yahoo.com>
2018-04-15 17:09:29 +0545

    refactor

---
+++ clustering/base.py
@@ -0,0 +1,26 @@
+class NotImplementedError(Exception):
+    pass
+
+
+class ClusteringOptions:
+    """
+    Object that stores info about how the model is created
+    """
+    def __init__(
+            self, n_clusters=8, use_hashing=True, use_idf=False,
+            n_components=None, minibatch=False, n_features=1000,
+            store_X=False
+            ):
+        self.n_clusters = n_clusters
+        self.n_features = n_features
+        self.use_hashing = use_hashing
+        self.use_idf = use_idf
+        self.n_components = n_components  # for dimensionality reduction
+        self.minibatch = minibatch
+        self.store_X = store_X
+
+
+class GenericClustering:
+
+    def perform_cluster(self, documents):
+        raise NotImplementedError
