4a26fd3fa1f8659571b5a66e082dc86cc4286641
bewakes <spirit_bibek@yahoo.com>
2018-04-18 14:07:22 +0545

    doc2vec script require group_id

--- clustering/scripts/create_doc2vecs.py
+++ clustering/scripts/create_doc2vecs.py
@@ -6,19 +6,19 @@ from clustering.models import Doc2VecModel
 
 def main(*args, **kwargs):
     modelname = kwargs.get('model_name')
-    modelversion = kwargs.get('model_version')
+    group_id = kwargs.get('group_id')
     if not modelname:
         print("Model name not provided. Provide it with: --model_name <name>")
         return
-    if not modelversion:
-        print("Model version not provided. Provide it with: --model_version\
+    if not group_id:
+        print("Group id not provided. Provide it with: --group_id\
 <version>")
         return
     model = create_doc2vec_model()
     try:
         with transaction.atomic():
-            dbmodel = Doc2VecModel.new(model, modelname, modelversion)
-            print("Doc2VecModel with version {} and name {} has been successfully created".format(
+            dbmodel = Doc2VecModel.new(model, modelname, group_id)
+            print("Doc2VecModel with group id {} and name {} has been successfully created".format(
                 dbmodel.name, dbmodel.version
             ))
     except Exception as e:
