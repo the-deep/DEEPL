5bfa1905249b45689ab561ae22f008b42e4b4a81
bewakes <spirit_bibek@yahoo.com>
2018-04-20 12:11:56 +0545

    update script for optimal clusters

--- clustering/scripts/optimal_clusters.py
+++ clustering/scripts/optimal_clusters.py
@@ -1,23 +1,37 @@
 from sklearn.metrics import silhouette_score
 
-from clustering.kmeans_docs import KMeansDocs, ClusteringOptions
+from clustering.base import ClusteringOptions
+from clustering.kmeans_docs import KMeansDocs
+from clustering.kmeans_doc2vec import KMeansDoc2Vec
+from clustering.models import Doc2VecModel
 from classifier.models import ClassifiedDocument
 from helpers.utils import timeit
 
 
 @timeit
-def find_optimal_clusters(min_clusters=2, max_clusters=20):
-    # fetch texts first
-    docs = ClassifiedDocument.objects.all().values('id', 'text')
-    texts = list(map(lambda x: x['text'], docs))
+def find_optimal_clusters(
+        min_clusters=2,
+        max_clusters=20,
+        CLUSTER_CLASS=KMeansDocs,
+        doc2vec_group_id=None):
     max_silhouette = -1.0  # to compare with
     clusters = 1
+    if CLUSTER_CLASS == KMeansDocs:
+        docs = ClassifiedDocument.objects.all().values('id', 'text')
+        texts = list(map(lambda x: x['text'], docs))
+        cluster_params = texts
+    elif CLUSTER_CLASS == KMeansDoc2Vec:
+        # get Doc2VecModel
+        doc2vecmodel = Doc2VecModel.objects.get(group_id=doc2vec_group_id)
+        cluster_params = [x for x in doc2vecmodel.model.docvecs]
+    else:
+        raise Exception("Invalid class")
     for n in range(min_clusters, max_clusters + 1):
         if n > len(texts) - 1:
             break
         options = ClusteringOptions(n_clusters=n, store_X=True)
         k_means = KMeansDocs(options)
-        kmeans_model = k_means.perform_cluster(texts)
+        kmeans_model = k_means.perform_cluster(cluster_params)
         labels = kmeans_model.model.labels_
         silhouette = silhouette_score(k_means.X, labels, metric='euclidean')
         if silhouette >= max_silhouette:
@@ -27,6 +41,7 @@ def find_optimal_clusters(min_clusters=2, max_clusters=20):
 
 
 def main(*args, **kwargs):
+    cluster_method = kwargs.get('cluster_method')
     try:
         min_clusters = int(kwargs['min_clusters'])
         if min_clusters < 0:
@@ -46,6 +61,19 @@ def main(*args, **kwargs):
         kargs['min_clusters'] = min_clusters
     if max_clusters:
         kargs['max_clusters'] = max_clusters
+    # clustering method
+    if not cluster_method or cluster_method == 'bow':
+        kargs['CLUSTER_CLASS'] = KMeansDocs
+    elif cluster_method == 'doc2vec':
+        kargs['CLUSTER_CLASS'] = KMeansDoc2Vec
+        doc2vec_group_id = kwargs.get('doc2vec_group_id')
+        if not doc2vec_group_id:
+            print('Provide --doc2vec_group_id for clustring with doc2vec')
+            return
+        kargs['doc2vec_group_id'] = doc2vec_group_id
+    else:
+        print("Invalid cluster method. Valid methods: --cluster_method=[doc2vec|bow]")
+        return
 
     optimal, score = find_optimal_clusters(**kargs)
     print("The optimal number of clusters for dataset is {} with silhouette score of {}".format(optimal, score))
