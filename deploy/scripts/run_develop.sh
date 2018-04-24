4ef800cf8598b221b7f0afbc58ff7bd1bd7bd5cd
bewakes <spirit_bibek@yahoo.com>
2018-04-24 18:10:43 +0545

    deployment scripts update

--- deploy/scripts/run_develop.sh
+++ deploy/scripts/run_develop.sh
@@ -10,8 +10,13 @@ cd $ROOT_DIR
 . /venv/bin/activate
 pip3 install -r requirements.txt
 export NLP_INDICES_PATH=/code/nlp_indices/
-yarn install
-/code/node_modules/webpack-cli/bin/webpack.js 
-python manage.py collectstatic
-python manage.py migrate --no-input
-python manage.py runserver 0.0.0.0:8000
+if [[ $INSTANCE_TYPE != 'CELERY' ]]; then
+    yarn install
+    /code/node_modules/webpack-cli/bin/webpack.js 
+    python manage.py collectstatic
+    python manage.py migrate --no-input
+    python manage.py runserver 0.0.0.0:8000
+else
+    celery -A deepl worker -l info
+fi
+
