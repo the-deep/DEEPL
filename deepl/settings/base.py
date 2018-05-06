39e84e39d63f389f33e0d9a8871b0917bfd09455
bewakes <spirit_bibek@yahoo.com>
2018-05-06 17:57:38 +0545

    add default versioning

--- deepl/settings/base.py
+++ deepl/settings/base.py
@@ -86,6 +86,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'deepl.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning'
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
