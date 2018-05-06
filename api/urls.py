27651c1c6c5309126c51ed42a9edc925b6c34b45
bewakes <spirit_bibek@yahoo.com>
2018-05-06 13:48:58 +0545

    prepend versions to urls

--- api/urls.py
+++ api/urls.py
@@ -15,22 +15,35 @@ from api.views import (
 )
 
 urlpatterns = [
-    url(
-        r'^(?P<version>[a-z0-9A-Z\.]+)/classify/$',
-        DocumentClassifierView.as_view()
-    ),
+    url(r'^classify/$', DocumentClassifierView.as_view()),
     url(r'topic-modeling/$', TopicModelingView.as_view()),
     url(r'ner/$', NERView.as_view()),
     url(r'ner-docs/$', NERWithDocIdView.as_view()),
     url(r'versions/$', ApiVersionsView.as_view()),
     url(r'keywords-extraction/$', KeywordsExtractionView.as_view()),
-    url(
-        r'^(?P<version>[a-z0-9A-Z\.]+)/recommendation/$',
-        RecommendationView.as_view()
-    ),
+    url(r'^recommendation/$', RecommendationView.as_view()),
     url(r'^(?P<entity>[a-zA-Z\.]+)/correlation/$', CorrelationView.as_view()),
     url(r'^similarity/$', DocsSimilarityView.as_view()),
     url(r'^similardocs/$', SimilarDocsView.as_view()),
     url(r'^clustering/$', ClusteringView.as_view()),
     url(r'^doc/$', ClassifiedDocumentView.as_view())
+]
+
+VERSION_PATTERN = '(?P<version>[a-z0-9A-Z\.]+)'
+
+
+def prepend_version_if_not(pattern):
+    newpattern = pattern
+    if VERSION_PATTERN not in pattern:
+        newpattern = '^{}/{}'.format(
+            VERSION_PATTERN,
+            pattern.replace('^', '').lstrip('/')
+        )
+    return newpattern
+
+
+# now prepend version pattern to the url patterns not having version pattern
+urlpatterns += [
+    url(prepend_version_if_not(x._regex), x.callback)
+    for x in filter(lambda x: VERSION_PATTERN not in x._regex, urlpatterns)
 ]
