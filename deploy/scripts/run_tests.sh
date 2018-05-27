d578d0c7a0f0a628b13d0f451fa9fe5a88fdfdd5
bewakes <spirit_bibek@yahoo.com>
2018-05-27 14:45:52 +0545

    update test script

--- deploy/scripts/run_tests.sh
+++ deploy/scripts/run_tests.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 . /venv/bin/activate
 
 if [ "$CI" == "true" ]; then
-    . /venv/bin/activate && \
-        pip install -r requirements.txt &&  \
-        python -c "import nltk; nltk.download('stopwords')" && \
-        python -c "import nltk; nltk.download('wordnet')" && \
-        python -c "import nltk; nltk.download('punkt')" && \
-        python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
+    . /venv/bin/activate
+    pip install -r requirements.txt
+    python -c "import nltk; nltk.download('stopwords')"
+    python -c "import nltk; nltk.download('wordnet')"
+    python -c "import nltk; nltk.download('punkt')"
+    python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
     pip install codecov
     coverage run -m pytest
     coverage report
