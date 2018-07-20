c292e98b9cc70d5ff63c8114ef6b2e212f445f3b
bewakes <spirit_bibek@yahoo.com>
2018-07-20 16:47:23 +0545

    update script for celery service startup

--- deploy/scripts/celery.sh
+++ deploy/scripts/celery.sh
@@ -2,10 +2,10 @@
 
 . /venv/bin/activate
 pip3 install -r requirements.txt
+rm celerybeat.pid
 
 # Celery Beat
 celery -A deepl beat -l info &
 # Celery Worker
 celery -A deepl worker -l info
 
-rm celerybeat.pid
