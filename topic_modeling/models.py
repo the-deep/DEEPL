ef6b69e9ac3e6ec3ea0c83502bfaebe214e15742
bewakes <spirit_bibek@yahoo.com>
2018-08-07 15:51:44 +0545

    Add tasks for topic modeling

--- topic_modeling/models.py
+++ topic_modeling/models.py
@@ -8,7 +8,7 @@ class TopicModelingModel(BaseModel):
     """
     Model storing topic modeling data
     """
-    group_id = models.CharField(max_length=50, unique=True)
+    group_id = models.CharField(max_length=50)
     data = JSONField(default={})
     number_of_topics = models.IntegerField()
     keywords_per_topic = models.IntegerField()
@@ -16,6 +16,11 @@ class TopicModelingModel(BaseModel):
     ready = models.BooleanField(default=False)
     extra_info = JSONField(default={})
     last_run_on = models.DateField(null=True)
+
+    class Meta:
+        unique_together = (
+            'group_id', 'number_of_topics', 'keywords_per_topic', 'depth'
+        )
 
     def __str__(self):
         return self.group_id
